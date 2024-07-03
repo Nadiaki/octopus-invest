b0d6d891ec1ad3854796bd5dd5a0a5ad69a3fce4
Nadia Antoniou <antoniounadia@gmail.com>
2024-07-03 20:15:35 +0200

    switch to repo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,31 +25,21 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-AppDataSource.initialize().then(async () => {
 
-  console.log("Inserting a new user into the database...")
-  const user = new User()
-  user.firstName = "Timber"
-  user.lastName = "Saw"
-  user.age = 25
-  await AppDataSource.manager.save(user)
-  console.log("Saved a new user with id: " + user.id)
+const course = new Course()
+course.title = "Intro to Investing"
+course.author = "Author 1"
+course.lessons = 10
+course.likes = 100
+course.image = "/images/book.jpg"
 
-  console.log("Loading users from the database...")
-  const users = await AppDataSource.manager.find(User)
-  console.log("Loaded users: ", users)
+const courseRepository = AppDataSource.getRepository(Course)
 
-  const course = new Course()
-  course.title = "Intro to Investing"
-  course.author = "Author 1"
-  course.lessons = 10
-  course.likes = 100
-  course.image = "/images/book.jpg"
-  await AppDataSource.manager.save(course)
-  const courses = await AppDataSource.manager.find(Course)
-  console.log("Loaded courses: ", courses)
+await courseRepository.save(course)
+const savedCourses = courseRepository.find()
+console.log("Loaded courses: ", savedCourses)
 
 
-}).catch(error => console.log(error))
+
 
 
